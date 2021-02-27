5a81ad70e59035d53bef879ad4a54c61dd77fb84
KRK <kay@krknet.de>
2021-02-27 17:14:37 +0100

    Feat: Important Changes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <div class="container mx-auto py-6">
+    <div class="container mx-auto py-14 space-y-14">
+      <h1 class="text-center text-3xl tracking-wider mb-10 font-semibold uppercase">Should I automate It?</h1>
       <div class="flex justify-center -mx-6">
         <div class="mx-6 space-y-4 py-4 px-4 max-w-xs border rounded-sm bg-white shadow-sm border-gray-400">
           <h3 class="text-center font-medium text-lg tracking-wider leading-6 mb-5">Task</h3>
@@ -33,9 +34,9 @@
           <h3 class="text-center font-medium text-lg tracking-wider leading-6 mb-5">Automation</h3>
           <v-input
             title="Duration"
-            hint="in Minutes"
+            hint="in Hours"
             type="integer"
-            post="min"
+            post="hours"
             v-model="autoDuration"
             required
           />
@@ -50,28 +51,45 @@
         </div>
       </div>
 
-      <div class="my-10 py-4 px-4 border rounded-sm bg-white shadow-sm border-gray-400">
-        <h3 class="text-center font-medium text-lg tracking-wider leading-6 mb-5">Amortization in Weeks</h3>
-        <table class="w-full tabular-nums leading-none">
+      <div class="py-4 border rounded-sm bg-white shadow-sm border-gray-400">
+        <h3 class="px-4 text-center font-medium text-lg tracking-wider leading-6 mb-5">Amortization in Weeks</h3>
+        <table class="w-full table-fixed tabular-nums leading-none whitespace-nowrap">
           <tbody>
-            <tr class="border-b">
-              <td class="px-2 py-1 border-r"></td>
+            <tr>
+              <td class="px-2 w-10 py-2"></td>
+              <td class="px-2 py-2 border-r"></td>
+              <td :colspan="autoDurationSpread.length" class="text-center px-2 py-1 font-medium">Automation Duration</td>
+            </tr>
+            <tr class="border-b font-medium">
+              <td class="px-2 py-2"></td>
+              <td class="px-2 py-2 border-r"></td>
               <td
                 v-for="dur of autoDurationSpread"
                 :key="dur"
-                class="text-center font-medium px-2 py-1"
+                class="text-center px-2 py-2"
                 v-text="dur"
               />
             </tr>
             <tr
-              v-for="rec of taskRecurrenceSpread"
+              v-for="(rec, i) of taskRecurrenceSpread"
               :key="rec"
             >
-              <td class="px-2 py-1 text-right border-r font-medium" v-text="rec" />
+              <td
+                v-if="i === 0"
+                class="px-0 py-0 w-10 relative"
+                :rowspan="taskRecurrenceSpread.length"
+              >
+                <div class="relative w-full h-full bg-red-500">
+                  <div class="absolute h-full w-10 z-10 flex flex-col justify-center items-center">
+                    <div class="absolute font-medium transform -rotate-90">Recurrences per Week</div>
+                  </div>
+                </div>
+              </td>
+              <td class="px-2 py-2 text-right border-r font-medium" v-text="rec" />
               <td
                 v-for="dur of autoDurationSpread"
                 :key="dur"
-                class="text-center px-2 py-1"
+                class="text-center px-2 py-2"
                 v-html="getAmortization(rec, dur)"
               />
             </tr>
@@ -86,6 +104,9 @@
   import vInput from './components/v-input.vue'
   import { formatNum } from '@krknet/fe/helpers'
 
+  import { scaleLinear } from 'd3-scale'
+  console.log(scaleLinear)
+
   export default {
     components: { vInput },
     data () {
@@ -93,28 +114,25 @@
         taskDuration: 5, // Duration in Minutes
         taskRecurrence: 5, // Recurrences per KW
         taskHourlyCosts: 20,
-        autoDuration: 60, // Duration in Minutes
+        autoDuration: 5, // Duration in Hours
         autoHourlyCosts: 100
       }
     },
     computed: {
       taskRecurrenceSpread () {
-        return [0.5, 0.75, 1, 1.25, 1.5].map(d => d * this.taskDuration)
+        return scaleLinear().domain([this.taskRecurrence * 0.5, this.taskRecurrence * 1.5]).nice().ticks(5)
       },
       autoDurationSpread () {
-        return [0.5, 0.75, 1, 1.25, 1.5].map(d => Math.round(d * this.autoDuration))
+        return scaleLinear().domain([this.autoDuration * 0.5, this.autoDuration * 1.5]).nice().ticks(5)
       },
       taskCostPerMinute () {
         return this.taskHourlyCosts / 60
-      },
-      autoCostPerMinute () {
-        return this.autoHourlyCosts / 60
       }
     },
     methods: {
       getAmortization (recurrence, duration) {
         const task = this.taskDuration * this.taskCostPerMinute * recurrence
-        const auto = duration * this.autoCostPerMinute
+        const auto = duration * this.autoHourlyCosts
         return formatNum(Math.ceil(auto / task))
       }
     }
